1faa06c2063b49ec4c7eae380ece206f905bcfb7
Artur Quirino <arturquirino6@gmail.com>
2019-03-22 06:55:25 -0300

    Create a Xamarin solution based on a template given by visual studio

---
+++ UpcomingMovies/UpcomingMovies/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UpcomingMovies.Models;
+
+namespace UpcomingMovies.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
